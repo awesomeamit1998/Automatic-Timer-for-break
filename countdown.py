4a3cd289fc73a7e3b1b6b386b80a767ff4c7a19a
Amit Jena <40141937+awesomeamit1998@users.noreply.github.com>
2019-06-17 01:53:38 +0530

    Add files via upload

---
+++ countdown.py
@@ -0,0 +1,12 @@
+import time
+
+def countdown(t):
+    print("Hey Amit its time for a break listen too some songs")
+    while t:
+        mins, secs = divmod(t, 60)
+        timeformat = '{:02d}:{:02d}'.format(mins, secs)
+        print(timeformat, end='\r')
+        time.sleep(1)
+        t -= 1
+    print('Goodbye!\n\n\n\n\n')
+countdown(240)
